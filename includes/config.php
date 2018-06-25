651593bf5d9cfcccf27dcc45c003a825555c56e5
Yusuf Kaka <yusufk@gmail.com>
2018-06-25 20:27:31 +0000

    customised for ebilal - removed hostapd config

--- includes/config.php
+++ includes/config.php
@@ -19,9 +19,9 @@ define('RASPI_OPENVPN_SERVER_CONFIG', '/etc/openvpn/server.conf');
 define('RASPI_TORPROXY_CONFIG', '/etc/tor/torrc');
 
 // Optional services, set to true to enable.
-define('RASPI_HOTSPOT_ENABLED', true );
-define('RASPI_NETWORK_ENABLED', true );
-define('RASPI_DHCP_ENABLED', true );
+define('RASPI_HOTSPOT_ENABLED', false );
+define('RASPI_NETWORK_ENABLED', false );
+define('RASPI_DHCP_ENABLED', false );
 define('RASPI_OPENVPN_ENABLED', false );
 define('RASPI_TORPROXY_ENABLED', false );
 define('RASPI_CONFAUTH_ENABLED', true );
